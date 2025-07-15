04a058d1ed3177746cf9e7966b7470163977dbc0
Nick Dimizas <nikkosdim@gmail.com>
2025-07-15 21:38:10 +0300

    chore: Set up and configure ESLint and Prettier

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,24 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import app from "./app";
-import { error } from "console";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+import app from './app';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URI =
-  process.env.MONGO_URI || "mongodb://localhost:27017/mydatabase";
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/mydatabase';
 
 mongoose
   .connect(MONGO_URI)
   .then(() => {
-    console.log("Connection to MongoDB established");
+    console.log('Connection to MongoDB established');
     app.listen(PORT, () => {
-      console.log("Server is up and running");
+      console.log('Server is up and running');
       console.log(`Access it at: http://localhost:${PORT}`);
     });
   })
   .catch((error) => {
-    console.log("Failed to connect to MongoDB:", error);
+    console.log('Failed to connect to MongoDB:', error);
     process.exit(1);
   });
 
